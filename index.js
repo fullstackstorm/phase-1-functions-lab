da651ca4d573d1ebfb41cccfc5c590f3fede8219
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-03-24 16:08:56 -0700

    Calculates distance and price for the knock-off Uber.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const hqStreet = 42;
+
+const distanceFromHqInBlocks = (pickupLocation) => Math.abs(pickupLocation - hqStreet);
+
+const distanceFromHqInFeet = (pickupLocation) => distanceFromHqInBlocks(pickupLocation) * 264;
+
+const distanceTravelledInFeet = (start, end) => Math.abs(start - end) * 264;
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if(distance <= 400){
+        return 0.00;
+    }
+    else if(distance <=  2000){
+        return (distance - 400) * 0.02;
+    }
+    else if(distance <= 2500){
+        return 25.00;
+    }
+    else{
+        return "cannot travel that far";
+    }
+}
